b7b46c04e64dd036bb3d9aae3388506bf76ecb0e
Artyom Abakumov <artem210966@gmail.com>
2024-10-18 18:37:01 +0300

    Add alg to save bar on disk

---
+++ backend/Algs/Graph.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <cmath>
+#include <memory>
+#include "Barcode/PrjBarlib/include/barcodeCreator.h"
+#include <random>
+
+
+#include "../MatrImg.h"
+#include "../MLSettings.h"
+#include "../Layers/layerInterface.h"
+#include "../Layers/Rasterlayers.h"
+
+// import MLSettings;
+// import MatrModule;
+import IAlgorithm;
+import AlgUtils;
+import SimpleBar;
+
+
+RetLayers exeBargraph(InOutLayer iol, const MLSettings& setting)
+{
+	bc::barstruct constr = getConstr(setting);
+	constr.coltype = bc::ColorType::gray;
+
+	using namespace std::string_literals;
+	BackImage main;
+
+	getSrcFromInput(iol, main);
+
+	std::unique_ptr<bc::Baritem> citem(bc::BarcodeCreator::create(main, constr));
+
+
+	StateBinFile::BinStateWriter writer;
+	writer.open("temp.bin");
+
+
+	auto& lines = citem->barlines;
+	writer.pArray(static_cast<buint>(lines.size()));
+	for(bc::barline* line : lines)
+	{
+		writer.pInt(line->start.getAvgUchar());
+		writer.pInt(line->m_end.getAvgUchar());
+	}
+
+
+	return {};
+}
+
+static AlgFuncRegister registerExeRestore("DisplayBarGraph", exeBargraph, mkSettingsType, "Preview");
